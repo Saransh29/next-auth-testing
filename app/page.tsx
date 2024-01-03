add69d3ef534c42ef997db263a5cb0844c8ac4fe
Saransh29 <saranshbibiyan234@gmail.com>
2024-01-04 00:05:48 +0530

    shadcn-ui , zod , forms

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,29 @@
-import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import LoginButton from "@/components/auth/loginButton";
+
+const PoppinsFont = Poppins({ subsets: ["latin"], weight: ["600"] });
 
 export default function Home() {
-  return <p className="text-5xl text-green-300">Next Auth Testing</p>;
+  return (
+    <main className="flex h-full flex-col items-center justify-center  bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-200 to-blue-200 ">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-5xl font-semibold drop-shadow-md",
+            PoppinsFont.className,
+          )}
+        >
+          🔐 Auth
+        </h1>
+        <p className="text-lg">Testing Simple Auth stuff</p>
+        <LoginButton>
+          <Button variant="secondary" size="lg">
+            Sign in
+          </Button>
+        </LoginButton>
+      </div>
+    </main>
+  );
 }
